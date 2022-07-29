4f66ce0f5d2fc1972b3e88b1d040fa32d1017331
GongDel <yolip51@naver.com>
2022-07-29 21:01:24 +0900

    Delete test/3o3-gongjojin directory

--- test/3o3-gongjojin/src/main/java/com/gongdel/gongjojin/repository/MemberRepository.java
+++
@@ -1,7 +0,0 @@
-package com.gongdel.gongjojin.repository;
-
-import com.gongdel.gongjojin.entity.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<Member, Long> {
-}
